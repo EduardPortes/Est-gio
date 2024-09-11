5115c1f6edf89257dfc32f59878826fe5d35e915
EduardPortes <epcduas05@hotmail.com>
2024-09-11 12:14:20 -0300

    Ex3 indice, soma, k

---
+++ src/ex3/application/Program.java
@@ -0,0 +1,18 @@
+package ex3.application;
+
+public class Program {
+    public static void main(String[] args) {
+
+        int indice = 12;
+        int soma = 0;
+        int k = 1;
+
+        while (k < indice){
+            k += 1;
+            soma += k;
+        }
+
+        System.out.println(soma);
+
+    }
+}
